6b267b32cda31450636124f55e293c06c23046fc
timopix <57803428+timopix@users.noreply.github.com>
2023-02-24 21:44:14 -0700

    feat: update towel_sort, the algorithm is now faster

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+function towelSort(matrix) {
+  if (!matrix || matrix.length === 0) {
+    return [];
+  }
+
+  const sortedMatrix = matrix.map((row, index) => {
+    return index % 2 === 0 ? row : row.reverse();
+  });
+
+  return [].concat(...sortedMatrix);
 }
+
+module.exports = towelSort;
